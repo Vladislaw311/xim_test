614bc83befbc24fc15a97d3aa02496db6b733f6b
Vladislaw311 <94992834+Vladislaw311@users.noreply.github.com>
2024-03-31 17:36:09 +0300

    Add files via upload

---
+++ test_himia/test_himia/forms/start_window.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace test_himia
+{
+    partial class start_window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(start_window));
+            this.close_button = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // close_button
+            // 
+            this.close_button.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.close_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.close_button.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.close_button.FlatAppearance.BorderSize = 0;
+            this.close_button.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+            this.close_button.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.close_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.close_button.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.close_button.Location = new System.Drawing.Point(739, 9);
+            this.close_button.Margin = new System.Windows.Forms.Padding(0);
+            this.close_button.Name = "close_button";
+            this.close_button.Size = new System.Drawing.Size(87, 44);
+            this.close_button.TabIndex = 0;
+            this.close_button.Text = "close";
+            this.close_button.UseVisualStyleBackColor = false;
+            this.close_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(350, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 53);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // start_window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(835, 476);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.close_button);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "start_window";
+            this.Text = "start_window";
+            this.Load += new System.EventHandler(this.start_window_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button close_button;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ test_himia/test_himia/forms/start_window.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_himia
+{
+    public partial class start_window : Form
+    {
+        public start_window()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void start_window_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
